7d9f1ff59e2d1d09c929a086078fcaac244ee166
Creature547 <greminanya@yandex.ru>
2026-05-20 23:11:50 +0300

    before pass

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,7 +1,6 @@
 import {BackButtonComponent} from "../../components/back-button/index.js";
 import {MainPage} from "../main/index.js";
 import {AccordionComponent} from "../../components/accordion/index.js";
-// Добавляем импорты для работы с сетью
 import {ajax} from "../../modules/ajax.js";
 import {stockUrls} from "../../modules/stockUrls.js";
 
@@ -12,14 +11,14 @@ export class ProductPage {
     }
 
     getData() {
-        // Запрашиваем данные конкретно для этой карточки по ID
+
         ajax.get(stockUrls.getStockById(this.id), (data) => {
             this.renderData(data);
         });
     }
 
     renderData(data) {
-        // Этот HTML был у тебя в методе render, теперь мы рисуем его, когда данные пришли
+
         const html = `
             <div class="container mt-5">
                 <div id="back-btn-container"></div>
